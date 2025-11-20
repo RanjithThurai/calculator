7393b2239556034076568bea3dededffba3d0cf9
RanjithThurai <ranjithranjith9210@gmail.com>
2025-11-20 10:11:16 +0530

    build failure

--- test_app.py
+++ test_app.py
@@ -9,8 +9,8 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
-    
+
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert multiply(2, 5) == 6
     assert multiply(-1, 1) == -1
     assert multiply(0, 5) == 0
